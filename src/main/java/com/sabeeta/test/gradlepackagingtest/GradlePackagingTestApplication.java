ad791d03a5b45a5b585ca718e7bcacaaf785fcdf
bsabeeta <bsabeeta@vmware.com>
2024-02-12 14:38:11 +0530

    Initial code checkin

---
+++ src/main/java/com/sabeeta/test/gradlepackagingtest/GradlePackagingTestApplication.java
@@ -0,0 +1,13 @@
+package com.sabeeta.test.gradlepackagingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePackagingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlePackagingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabeeta/test/gradlepackagingtest/GradlePackagingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabeeta.test.gradlepackagingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlePackagingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
